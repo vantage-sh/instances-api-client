9d123f3e7ce2d92471ee201e8764cec54981bf87
Astrid Gealer <astrid@gealer.email>
2025-09-28 16:01:10 +0100

    add global aws types

--- src/apiTypings.ts
+++ src/apiTypings.ts
@@ -20,7 +20,43 @@ export type AzureInstance = {
 export type ChinaAWSRegions = "cn-north-1" | "cn-northwest-1" | "cn-north-1-pkx-1" | string;
 
 /** Defines the supported regions for global AWS region queries. */
-export type GlobalAWSRegions = string;
+export type GlobalAWSRegions =
+    "ap-northeast-1" |
+    "ap-south-1" |
+    "ap-southeast-1" |
+    "ap-southeast-2" |
+    "eu-central-1" |
+    "eu-west-1" |
+    "us-east-1" |
+    "us-east-2" |
+    "us-west-2" |
+    "sa-east-1" |
+    "us-gov-east-1" |
+    "us-gov-west-1" |
+    "us-west-1" |
+    "ap-northeast-2" |
+    "ap-northeast-3" |
+    "ca-central-1" |
+    "eu-west-2" |
+    "af-south-1" |
+    "ap-east-1" |
+    "ap-south-2" |
+    "ap-southeast-3" |
+    "ap-southeast-4" |
+    "ca-west-1" |
+    "eu-central-2" |
+    "eu-north-1" |
+    "eu-south-1" |
+    "eu-south-2" |
+    "eu-west-3" |
+    "il-central-1" |
+    "me-central-1" |
+    "me-south-1" |
+    "ap-east-2" |
+    "ap-southeast-5" |
+    "ap-southeast-7" |
+    "mx-central-1" |
+    string;
 
 /** Defines the supported regions for global Azure region queries. */
 export type GlobalAzureRegions = 
